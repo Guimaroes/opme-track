41bcaf70b527f75691aec66ada3554ab4bd60c60
Guimaroes <gabriel.guimaraes@universo.univates.br>
2024-09-22 22:41:12 -0300

    Home screen style

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,11 +3,12 @@ import { View, Text, Button } from 'react-native';
 
 const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ padding: 20 }}>
       <Button
         title="Registrar Entrega de Materiais"
         onPress={() => navigation.navigate('Registrar Entrega')}
       />
+      <View style={{ marginBottom: 20 }}></View>
       <Button
         title="Ver Entregas Salvas"
         onPress={() => navigation.navigate('Entregas Salvas')}
